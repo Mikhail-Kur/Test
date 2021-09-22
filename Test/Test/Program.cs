7735f2b8d262e07b223f4b71efa3b2c864bdafc2
Mikchail Kurkov <kurkov190801@mail.ru>
2021-09-22 11:26:37 +0700

    delited getrequest, final commit

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -12,85 +12,58 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            byte[] hash;
-            string passwor;
-            string Req;
-            string Pass= "8e49ff607b1f46e1a5e8f6ad5d312a80";
-            WebRequest request = WebRequest.Create("http://api.pixlpark.com/oauth/requesttoken");
-            request.Method = "POST";
-
-            Stream dataStream = request.GetRequestStream();
-
-            // Get the response.
-            WebResponse response = request.GetResponse();
-            // Display the status.
-            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-            using (dataStream = response.GetResponseStream())
-            {
-                // Open the stream using a StreamReader for easy access.
-                StreamReader reader = new StreamReader(dataStream);
-                // Read the content.
-                string responseFromServer = reader.ReadToEnd();
-                dynamic resp = JObject.Parse(responseFromServer);
-                // Display the content.
-                Console.WriteLine(resp.RequestToken);
-                Req = resp.RequestToken;
-                Console.WriteLine(Req + Pass);
-            }
-            response.Close();
-
-            // Close the response.
-            request= WebRequest.Create("http://api.pixlpark.com/oauth/accesstoken");
-            request.Method="GET";
-            passwor = Req + Pass;
-            SHA1 sHA = SHA1.Create();
-            hash = sHA.ComputeHash(Encoding.Default.GetBytes(passwor));
-            string acces = "";
-            for (int i = 0; i < hash.Length; i++)
-            {
-                acces += hash[i];
-            }
-            request.Headers.Add("oauth_token",Req);
-            request.Headers.Add("grant_type", "POST");
-            request.Headers.Add("username", "38cd79b5f2b2486d86f562e3c43034f8");
-
-            request.Headers.Add("password", acces);
+            
+                
+                string passwor;
+                string Req;
+                string Pass = "8e49ff607b1f46e1a5e8f6ad5d312a80";
+                WebRequest request = WebRequest.Create("http://api.pixlpark.com/oauth/requesttoken");
+                request.Method = "GET";
+                WebResponse response = request.GetResponse();
 
 
-            // Get the response.
-            response = request.GetResponse();
-            dataStream = response.GetResponseStream();
-            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-            using (dataStream = response.GetResponseStream())
-            {
-                // Open the stream using a StreamReader for easy access.
-                StreamReader reader = new StreamReader(dataStream);
-                // Read the content.
-                string responseFrom = reader.ReadToEnd();
-                dynamic resp = JObject.Parse(responseFrom);
-                // Display the content.
-                Console.WriteLine(responseFrom+"   ==");
-            }   
+                Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(dataStream);
+                    string responseFromServer = reader.ReadToEnd();
+                    dynamic resp = JObject.Parse(responseFromServer);
+                    Console.WriteLine(resp.RequestToken);
+                    Req = resp.RequestToken;
 
-            /*
-            String id = "38cd79b5f2b2486d86f562e3c43034f8";
-            String secret = "8e49ff607b1f46e1a5e8f6ad5d312a80";
+                }
+                response.Close();
 
-            var client = new RestClient("http://api.pixlpark.com/oauth/requesttoken");
-            var request = new RestRequest(Method.POST);
-            request.AddHeader("cache-control", "no-cache");
-            request.AddHeader("content-type", "application/x-www-form-urlencoded");
-            request.AddParameter("application/x-www-form-urlencoded", "grant_type=client_credentials&scope=all&client_id=" + id + "&client_secret=" + secret, ParameterType.RequestBody);
-            IRestResponse response = client.Execute(request);
+                
+                request = WebRequest.Create("http://api.pixlpark.com/oauth/accesstoken");
+                request.Method = "GET";
+                passwor = Req + Pass;
+                Console.WriteLine(passwor);
+                SHA1 sha = SHA1.Create();
+                byte[] bytes = new ASCIIEncoding().GetBytes(passwor);
+                sha.ComputeHash(bytes);
+                string acces = Convert.ToBase64String(sha.Hash);
+                acces = acces.Remove(acces.Length-1);
+                Console.WriteLine(acces);
+                request.Headers.Add("oauth_token", Req);
+                request.Headers.Add("username", "38cd79b5f2b2486d86f562e3c43034f8");
+                request.Headers.Add("password", acces);
+                request.Headers.Add("grant_type", "api");
 
-            dynamic resp = JObject.Parse(response.Content);
-            String token = resp.RequestToken;
-            Console.WriteLine(response.Cookies);
-            Console.WriteLine(response.Headers);
 
-            var request1 = new GetRequest("http://api.pixlpark.com/orders", token,id,secret);
-            request1.Run();
-            */
+                response = request.GetResponse();
+                Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    
+                    StreamReader reader = new StreamReader(dataStream);
+                    string responseFrom = reader.ReadToEnd();
+                    dynamic resp = JObject.Parse(responseFrom);
+                    Console.WriteLine(resp);
+                }
+                response.Close();
+
+            
 
         }
     }
