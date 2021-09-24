6ed7160f0b8c7d27c6bdd4ed654b646ec6a94d12
Mikchail Kurkov <kurkov190801@mail.ru>
2021-09-24 21:19:22 +0700

    add class "GetRequest" try to proxy request

---
+++ Test/Test/GetRequest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Test
+{
+    public class GetRequest
+    {
+
+
+        HttpWebRequest _request;
+        string _address;
+
+        public Dictionary<string, string> Headers { get; set; }
+
+        public string Response { get; set; }
+        public string Oauth_token { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Grant_type { get; set; }
+        public WebProxy Proxy { get; set; }
+
+        public GetRequest(string address)
+        {
+            _address = address;
+            Headers = new Dictionary<string, string>();
+        }
+
+        public void Run(CookieContainer cookieContainer)
+        {
+            _request = (HttpWebRequest)WebRequest.Create(_address);
+            _request.Method = "Get";
+            _request.CookieContainer = cookieContainer;
+            _request.Proxy = Proxy;
+            _request.Headers.Add("oauth_token", Oauth_token);
+            _request.Headers.Add("username", Username);
+            _request.Headers.Add("password", Password);
+            _request.Headers.Add("grant_type", Grant_type);
+           
+            foreach (var pair in Headers)
+            {
+                _request.Headers.Add(pair.Key, pair.Value);
+            }
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+                var stream = response.GetResponseStream();
+                if (stream != null) Response = new StreamReader(stream).ReadToEnd();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+    }
+}
--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -12,11 +12,23 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            
-                
+            string sha1(string input)
+            {
+                byte[] hash;
+                using (var sha1 = new SHA1CryptoServiceProvider())
+                    hash = sha1.ComputeHash(Encoding.Unicode.GetBytes(input));
+                var sb = new StringBuilder();
+                foreach (byte b in hash) sb.AppendFormat("{0:x2}", b);
+                return sb.ToString();
+            }
+
+
+
                 string passwor;
                 string Req;
                 string Pass = "8e49ff607b1f46e1a5e8f6ad5d312a80";
+                string Username = "38cd79b5f2b2486d86f562e3c43034f8";
+
                 WebRequest request = WebRequest.Create("http://api.pixlpark.com/oauth/requesttoken");
                 request.Method = "GET";
                 WebResponse response = request.GetResponse();
@@ -34,36 +46,23 @@ namespace Test
                 }
                 response.Close();
 
-                
-                request = WebRequest.Create("http://api.pixlpark.com/oauth/accesstoken");
-                request.Method = "GET";
-                passwor = Req + Pass;
-                Console.WriteLine(passwor);
-                SHA1 sha = SHA1.Create();
-                byte[] bytes = new ASCIIEncoding().GetBytes(passwor);
-                sha.ComputeHash(bytes);
-                string acces = Convert.ToBase64String(sha.Hash);
-                acces = acces.Remove(acces.Length-1);
-                Console.WriteLine(acces);
-                request.Headers.Add("oauth_token", Req);
-                request.Headers.Add("username", "38cd79b5f2b2486d86f562e3c43034f8");
-                request.Headers.Add("password", acces);
-                request.Headers.Add("grant_type", "api");
+
+            passwor = Req + Pass;
+            Console.WriteLine(passwor);
+            string hash = sha1(passwor);
+
+            var cookieContainer = new CookieContainer();
+            var getRequest = new GetRequest("http://api.pixlpark.com/oauth/accesstoken");
+            getRequest.Grant_type = "api";
+            getRequest.Oauth_token = Req;
+            getRequest.Username = Username;
+            getRequest.Password = hash;
+            getRequest.Proxy = new WebProxy("127.0.0.1:8888");
+            getRequest.Run(cookieContainer);
+            Console.WriteLine(getRequest.Response);
+           
 
 
-                response = request.GetResponse();
-                Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-                using (Stream dataStream = response.GetResponseStream())
-                {
-                    
-                    StreamReader reader = new StreamReader(dataStream);
-                    string responseFrom = reader.ReadToEnd();
-                    dynamic resp = JObject.Parse(responseFrom);
-                    Console.WriteLine(resp);
-                }
-                response.Close();
-
-            
 
         }
     }
