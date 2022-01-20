399b826ed97a0ac5be66bcc4a3121d1ef9edc02a
Mikchail Kurkov <kurkov190801@mail.ru>
2022-01-20 16:19:10 +0700

    can`t take access token

---
+++ Leson/Leson/Models/SiteResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leson.Models
+{
+    public class SiteResponse
+    {
+
+        public string reqtoken { get; set; }
+        public string accestoken { get; set; }
+
+
+
+    }
+}
